86197ab318ba13701f533d7c893a394d20ff7f8d
RehanShaikh007 <rehanmukhtarshaikh7@gmail.com>
2025-01-15 11:01:02 +0530

    changes in Route file

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
-import User from "../models/UserModel.js";
+import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
